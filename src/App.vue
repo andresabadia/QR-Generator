3e6efd80335299027de342b0da6b319cc01674cc
andresabadia <andres.abadia@id-ex.de>
2019-05-09 17:18:29 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <QR></QR>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import QR from './components/QR.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    QR
   }
 }
 </script>
@@ -21,8 +22,10 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+/* .dont-print{
+  display: block;
+} */
+
+
 </style>
---
+++ src/components/QR.vue
@@ -0,0 +1,158 @@
+<template>
+  <div class="qr">    
+    <div class="dont-print">
+      <div class="keychain-properties">Keychain Width (cm) <input type="number" step="0.1" v-model="keychainWidth"></div>
+      <div class="keychain-properties">Keychain Height (cm) <input type="number" step="0.1" v-model="keychainHeight"></div>
+      <div class="keychain-properties">QR width and height (cm) <input type="number" step="0.1" v-model="qrWidth"></div>
+      <div class="keychain-properties">Select logo <input type="file" accept="image/png" @change="setLogo($event)"></div>
+      <div class="keychain-properties">Logo width and height (cm) <input type="number" step="0.1" v-model="logoWidth"></div>
+      <div class="keychain-properties">Margin button (cm) <input type="number" step="0.1" v-model="marginButtom"></div>
+      <div class="keychain-properties">Title <input type="text" v-model="title"></div>
+      <div class="keychain-properties">Title font size (pt) <input type="number" step="0.5" v-model="titleSize"></div>
+      <div class="keychain-properties">Title margin button (cm) <input type="number" step="0.1" v-model="titleMarginBottom"></div>
+      <div class="keychain-properties">String font size (pt) <input type="number" step="0.5" v-model="stringSize"></div>
+      <div class="keychain-properties">Gym ID <input type="text" v-model="dataGymID"> Data preview: {{data(9999)}}</div>
+      <div class="keychain-properties">Data quantity <input type="number" step="1" v-model="dataQuantity" @change="setData()"></div>
+      <div class="keychain-properties"><button @click="print()">PRINT</button></div>
+      <div class="keychain" :style="'width:' + keychainWidth + 'cm; height:' + keychainHeight + 'cm;'">
+        <div class="qr-code-container" :style="'margin-bottom:' + marginButtom + 'cm'">
+          <img class="qr-code" :style="'width:' + qrWidth + 'cm'" :src="'https://api.qrserver.com/v1/create-qr-code/?size=300x300&data='+data(9999)+'&ecc=H&format=svg'" alt="" title="" />
+          <div class="logo logo-background" :style="'width:' + logoSize + 'cm; height:' + logoSize + 'cm;'"></div>
+          <img class="logo" :style="'width:' + logoWidth + 'cm'" alt="Vue logo" :src="logoSrc==''?'logo.png':logoSrc">
+        </div>
+        <div class="keychain-string" :style="'font-size:' + stringSize + 'pt'">ID: 9999</div>
+        <div class="keychain-title" :style="'font-size:' + titleSize + 'pt; margin-bottom:' + titleMarginBottom + 'cm'" >{{title}}</div>
+      </div>
+    </div>
+    <div class="print-me">
+      <div class="keychain" :style="'width:' + keychainWidth + 'cm; height:' + keychainHeight + 'cm;'" v-for="code in codes" :key="code">
+        <div class="qr-code-container" :style="'margin-bottom:' + marginButtom + 'cm'">
+          <img class="qr-code" :style="'width:' + qrWidth + 'cm'" :src="'https://api.qrserver.com/v1/create-qr-code/?size=300x300&data='+data(code)+'&ecc=H&format=svg'" alt="" title="" />
+          <div class="logo logo-background" :style="'width:' + logoSize + 'cm; height:' + logoSize + 'cm;'"></div>
+          <img class="logo" :style="'width:' + logoWidth + 'cm'" alt="Vue logo" :src="logoSrc==''?'logo.png':logoSrc">
+        </div>
+        <div class="keychain-string" :style="'font-size:' + stringSize + 'pt'">ID: {{code}}</div>
+        <div class="keychain-title" :style="'font-size:' + titleSize + 'pt; margin-bottom:' + titleMarginBottom + 'cm'" >{{title}}</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+      keychainWidth: 4.5,
+      keychainHeight: 6,
+      qrWidth:4,
+      logoSrc: "",
+      logoWidth:0.8,
+      marginButtom:0.2,
+      title: "Title",
+      titleSize:15,
+      titleMarginBottom: 0,
+      stringSize:10,
+      dataGymID:'zz',
+      dataQuantity: 5,
+      codes:[
+        1,2,3,4,5
+      ]
+
+    }
+  },
+  computed:{
+    logoSize(){
+      return this.qrWidth*7/29
+    }
+  },
+  methods:{
+    setLogo(ev){
+      if(ev.target.files && ev.target.files[0]){
+        const file = ev.target.files[0]
+        const reader = new FileReader()
+        reader.onload = e => {
+          // console.log('reader', e.target.result)
+          this.logoSrc = e.target.result
+        }
+        reader.readAsDataURL(file)
+      } else {
+        this.logoSrc =''
+      }
+      
+    },
+    data(code){
+      return '{"obj":"l","'+this.dataGymID+'id":'+code+'}'
+    },
+    setData(){
+      this.codes=[]
+      for(let i=0; i<this.dataQuantity; i++){
+        this.codes.push(i+1)
+      }
+    },
+    print(){
+      window.print()
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.qr{
+  text-align: center;
+}
+.print-me{
+  display: block;
+  /* display: none; */
+}
+.keychain-properties{
+  text-align: left;
+}
+.keychain{
+  border: 1px solid black;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column-reverse;
+}
+.print-me .keychain{
+  float: left;
+}
+.keychain-title{
+  font-weight: bold;
+}
+.qr-code-container{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+}
+.qr-code-container .logo{
+  position: absolute;
+}
+.logo-background{
+  background: white;
+ }
+
+
+@media print {
+  body{
+    margin:0;
+  }
+  .dont-print{
+    display: none;
+  }
+  .print-me{
+    display: block;
+  }
+  .keychain{
+    page-break-inside: avoid;
+  }
+  .keychain{
+    border: unset;
+  }
+}
+</style>
